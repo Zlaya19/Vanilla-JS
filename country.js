d7325d4590fb02733a57313d42409d908285dc5e
Zlatan Gavranovic <zlatangavranovic1906@gmail.com>
2021-08-20 19:00:35 +0200

    adding some features

--- country.js
+++ country.js
@@ -13,7 +13,16 @@ btn.addEventListener('click', function(){
            output = JSON.parse(this.responseText);
 
             console.log(output);
-            display.innerHTML = `<img src="${output[0].flag}"  <div class="headline"><h1> ${output[0].name} </h1><div>`;
+            display.innerHTML = `<img src="${output[0].flag}"
+            <div class="headline"><h1 class="h1Style"> ${output[0].name} </h1>
+    
+            <h1> <span>CAPITAL:</span> ${output[0].capital} </h1>
+            <h1> <span>POPULATION:</span>        ${output[0].population} </h1>
+            <h1 class="styleBorder"> <span>BORDERS: </span>       ${output[0].borders} </h1>
+            <h1> <span>LANGUAGE: </span>       ${output[0].languages[0].name} </h1>
+            <h1 class="h1Style2"> <span>CURRENCY: </span>     ${output[0].currencies[0].name} </h1>
+            <div>
+            `;
         }
     }
 
