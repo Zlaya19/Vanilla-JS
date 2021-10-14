ac1d090e6e35a95f8f738fd8257ff85cd59463de
Zlatan Gavranovic <zlatangavranovic1906@gmail.com>
2021-10-14 21:43:48 +0200

    update captcha app

--- captcha.js
+++ captcha.js
@@ -3,23 +3,57 @@ const btn = document.getElementById('btn');
 let input = document.getElementById('input');
 let output = '';
 
-function randomCaptcha() {
-    const captchaItems = [1,2,3,4,5,6,7,8,9,0,'a','b','c','d','e','f','u','v','w','x','y','z'];
-    for(let i = 0; i < 6; i++ ){
-        output += captchaItems[Math.floor(Math.random() * captchaItems.length)];
-    }
+    window.addEventListener('load', function(){
+
+        
+        for(let i = 0; i < 6; i++ ){
+            let paragraph = document.createElement('p');
+            paragraph.innerText = randomLetter();
+            paragraph.style.fontSize = randomFontsize();
+            paragraph.style.color = randomFontColor();
+            paragraph.style.transform = rotateParagraph();
+            paragraph.style.opacity = opacityParagraph();
+            paragraph.style.textTransform = BigSmall(); 
+            captchaDiv.appendChild(paragraph);
+
+        }  
+
+            btn.addEventListener('click', function(){
+                
+                if(input.value === captchaDiv.textContent){
+                    captchaDiv.innerHTML = '<h1>' + "Correct" + '</h1>';
+                }
+            });
+    })
     
+
+function randomLetter(){
+    const captchaItems = [1,2,3,4,5,6,7,8,9,0,'a','b','c','d','e','f','u','v','w','x','y','z'];
+    return captchaItems[Math.floor(Math.random() * captchaItems.length)];
+}
+   
+function randomFontsize(){
+    const fontsizeItems = ['36px','38px','40px','42px','44px','46px'];
+    return fontsizeItems[Math.floor(Math.random() * fontsizeItems.length)];
 }
 
-randomCaptcha();
+function randomFontColor(){
+    const fontcolorItems = ['gold','whitesmoke','coral'];
+    return fontcolorItems[Math.floor(Math.random() * fontcolorItems.length)];
+}
 
-console.log(output);
+function rotateParagraph(){
+    const rotateLetter = ["rotate(-1deg)", "rotate(5deg)","rotate(10deg)","rotate(15deg)"];
+    return rotateLetter[Math.floor(Math.random() * rotateLetter.length)];
+};
 
-captchaDiv.innerHTML = '<h1>' + output + '<h1>'
+function opacityParagraph(){
+    const opacityFont = ["0.2", "1", "1", "1", "1", "1", "1"];
+    return opacityFont[Math.floor(Math.random() * opacityFont.length)];
+};
 
-btn.addEventListener('click', function(){
-        
-    if(input.value === output){
-        captchaDiv.innerText = 'Correct';
-    }
-});
+function BigSmall(){
+    const capitalLower = ["lowercase","capitalize"];
+    return capitalLower[Math.floor(Math.random() * capitalLower.length)];
+};
+
